a4924223c3cfb62d3fde357bd793b4152320dbfb
lthurler <leothurler@gmail.com>
2023-08-17 12:11:15 -0300

    correcao de bug

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -3,71 +3,69 @@
 </script>
 
 <template>
-    <main>
-        <div class="container">
-            <div id="content">
-                <div id="logo">
-                    <img src="/src/assets/img/logo.png">
-                    <img src="/src/assets/img/brabos.png">
-                </div>
-
-                <div id="login">
-                    <div id="conta-img">
-                        <img src="/src/assets/img/Ativo 7.png">
-                        <img src="/src/assets/img/Ativo 8.png">
-                        <!-- <span>Minha Conta</span>                         -->
-                    </div>
-
-                    <div class="login-container">
-                       <div id="titulo">
-                          <div id="titulo-span">
-                            <span>Login</span>
-                          </div>
-                       </div>
-
-                          <div id="form-container">
-                            <form id="form">
-                                <div class="input" id="input-email">
-                                    <label for="email">E-mail:</label>
-                                    <input type="email" name="email" id="email">
-                                    <!-- <span class="msg">Valid email</span> -->
-                                </div>
-    
-                                <div class="input" id="input-senha">
-                                    <label for="password">Senha:</label>
-                                    <input type="password" name="password" id="senha">
-                                    <!-- <span class="msg">Incorrect password</span>  -->
-                                </div>
-    
-                                <button type="submit" id="login-button">Entrar</button>
-                            </form>
-                            
-                            <div id="esqueci">
-                                <a href="#">Esqueci a senha</a>
-                            </div>
-                          </div>
-                    </div>
-
-                    <div id="social-media">
-                        <a href="#"><img src="/src/assets/img/Ativo 2.png"></a>
-                        <a href="#"><img src="/src/assets/img/Ativo 3.png"></a>
-                        <a href="#"><img src="/src/assets/img/Ativo 4.png"></a>
-                        <a href="#"><img src="/src/assets/img/Ativo 5.png"></a>
-                    </div>
-                    
-                    <div id="url">
-                      <a href="#">WWW.AGENCIABRABOS.COM.BR</a>  
-                      <!-- <a href="#"><img src="/src/assets/img/Ativo 6.png"></a> -->
-                    </div>
-                </div>
-            </div>
+  <main>
+    <div class="container">
+      <div id="content">
+        <div id="logo">
+          <img src="/src/assets/img/logo.png">
+          <img src="/src/assets/img/brabos.png">
         </div>
-    </main>
 
+        <div id="login">
+          <div id="conta-img">
+            <img src="/src/assets/img/Ativo7.png">
+            <img src="/src/assets/img/Ativo8.png">
+            <!-- <span>Minha Conta</span>                         -->
+          </div>
+
+          <div class="login-container">
+            <div id="titulo">
+              <div id="titulo-span">
+                <span>Login</span>
+              </div>
+            </div>
+
+            <div id="form-container">
+              <form id="form">
+                <div class="input" id="input-email">
+                  <label for="email">E-mail:</label>
+                  <input type="email" name="email" id="email">
+                  <!-- <span class="msg">Valid email</span> -->
+                </div>
+
+                <div class="input" id="input-senha">
+                  <label for="password">Senha:</label>
+                  <input type="password" name="password" id="senha">
+                  <!-- <span class="msg">Incorrect password</span>  -->
+                </div>
+
+                <button type="submit" id="login-button">Entrar</button>
+              </form>
+
+              <div id="esqueci">
+                <a href="#">Esqueci a senha</a>
+              </div>
+            </div>
+          </div>
+
+          <div id="social-media">
+            <a href="#"><img src="/src/assets/img/Ativo2.png"></a>
+            <a href="#"><img src="/src/assets/img/Ativo3.png"></a>
+            <a href="#"><img src="/src/assets/img/Ativo4.png"></a>
+            <a href="#"><img src="/src/assets/img/Ativo5.png"></a>
+          </div>
+
+          <div id="url">
+            <a href="#">WWW.AGENCIABRABOS.COM.BR</a>
+            <!-- <a href="#"><img src="/src/assets/img/Ativo 6.png"></a> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-
 a {
   text-decoration: none;
   color: var(--white);
@@ -91,10 +89,10 @@ main {
   width: 100vw;
   height: 100vh;
   display: flex;
-  justify-content: space-between;  
+  justify-content: space-between;
 }
 
-#logo {  
+#logo {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -115,7 +113,7 @@ main {
   justify-content: center;
   align-items: center;
   padding-right: 11rem;
-  padding-bottom: 5rem;  
+  padding-bottom: 5rem;
 }
 
 #conta-img {
@@ -134,7 +132,7 @@ main {
   width: 55%;
   /* color: var(--white); */
   padding-top: .5rem;
-  padding-left: .7rem;  
+  padding-left: .7rem;
 }
 
 .login-container {
@@ -142,34 +140,34 @@ main {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: rgba(0,0,0,0.7);
+  background-color: rgba(0, 0, 0, 0.7);
   border-radius: 20px;
-  padding: 1rem 1rem 2rem 1rem;  
+  padding: 1rem 1rem 2rem 1rem;
 }
 
 #titulo {
-  border-bottom: 2px solid var(--white);  
-  width: 55%;  
+  border-bottom: 2px solid var(--white);
+  width: 55%;
 }
 
 #titulo-span {
   text-align: center;
   color: var(--white);
   font-size: 1.7rem;
-  padding-bottom: .4rem;  
+  padding-bottom: .4rem;
 }
 
-#form {  
+#form {
   padding-top: 1rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center; 
+  align-items: center;
 }
 
 .input {
   display: flex;
-  flex-direction: column;  
+  flex-direction: column;
 }
 
 #input-email {
@@ -181,19 +179,19 @@ main {
 }
 
 .input label {
-  color: var(--white);  
+  color: var(--white);
   padding-left: .3rem;
   font-size: 1rem;
 }
 
-.input input {  
+.input input {
   font-size: 1rem;
   padding: .5rem 2.5rem;
   background-color: var(--white);
   border: none;
   outline: none;
   border-radius: .40em;
-  color: var(--white);  
+  color: var(--white);
 }
 
 .input input:focus {
@@ -202,7 +200,7 @@ main {
 
 #login-button {
   padding: .1rem 3.3rem;
-  margin: 0 auto;  
+  margin: 0 auto;
   font-size: 1.2rem;
   font-family: Engrgotn;
   color: #000;
@@ -210,7 +208,7 @@ main {
   border: 1px solid hsl(var(--primary-hsl));
   border-radius: .35em;
   outline: none;
-  cursor: pointer;  
+  cursor: pointer;
 }
 
 #login-button:hover,
@@ -224,12 +222,12 @@ main {
 
 #esqueci a {
   font-size: .8rem;
-  text-decoration: underline;  
+  text-decoration: underline;
 }
 
 #social-media {
   padding-top: 1.5rem;
-  padding-left: 1.5rem;  
+  padding-left: 1.5rem;
   display: flex;
   justify-content: space-around;
   align-items: center;
@@ -245,5 +243,4 @@ main {
   padding-bottom: 1rem;
   font-size: 1.2rem;
 }
-
 </style>
